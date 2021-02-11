3dfa609ad4c9f991c32a2d0653761c2dee3d343b
HolynekK <e238109@metu.edu.tr>
2021-02-11 15:43:10 +0300

    main functionalities are added.

--- script.js
+++ script.js
@@ -1,118 +1,86 @@
-let display_val = 0;
-const number_buttons = document.querySelectorAll("#number");
-const del_button = document.querySelector("#del-button");
-const back = document.querySelector("#backspace")
-const operators = document.querySelectorAll("#bi_op");
-const equal = document.querySelector("#eq");
+const numbers = document.querySelectorAll("#number");
+const operators = document.querySelectorAll("#operator");
+const clear = document.querySelector("#clear-screen");
+const dot = document.querySelector("#dot");
+const del = document.querySelector("#backspace");
+const unary = document.querySelector("#plus-minus");
+const equal = document.querySelector("#equal");
+const display = document.querySelector("#screen");
 
-const screen_num = document.querySelector(".screen-num");
-
-let num_one = 0;
-let op = "";
-let num_two = 0;
-
-number_buttons.forEach((button) => {
-    button.addEventListener("click", ()=>{
-        screen_num.textContent += button.textContent;
+function shakeScreen(){
+    display.animate([
+        // keyframes
+        { transform: 'translateY(5px)' },
+        { transform: 'translateY(-5px)' }
+    ], {
+        // timing options
+        duration: 1,
+        iterations: 500
     });
-});
-
-del_button.addEventListener("click", ()=>{
-    screen_num.textContent = "";
-    clearCache();
-});
-
-operators.forEach((operator)=>{
-    operator.addEventListener("click", ()=>{
-        num_one = screen_num.textContent;
-        op = operator.textContent;
-        screen_num.textContent = "";
-    });
-});
-
-equal.addEventListener("click", ()=>{
-    num_two = screen_num.textContent;
-    num_one = operate(opType(op), parseInt(num_one), parseInt(num_two));
-    num_two = 0;
-    screen_num.textContent = num_one;
-    
-});
-
-function opType(given_op){
-    switch(given_op){
-        case "+":
-            return add;
-            break;
-        case "-":
-            return subs;
-            break;
-        case "/":
-            return divide;
-            break;
-        case "x":
-            return multiply;
-            break;
-        case "%":
-            return remainder;
-            break;
-        case "x**2":
-            return square;
-            break;
-        case "x**(0.5)":
-            return square_root;
-            break;
-        case "a**b":
-            return exponent;
-            break;
-        case "!":
-            return factorial;
-            break;
-    }
-         
-}
-
-
-// Functions for caluclator buttons 
-
-function add(a, b){
-    return a + b;
-}
-function subs(a,b){
-    return a - b;
-}
-function divide(a,b){
-    return a / b;
-}
-function multiply(a,b){
-    return a * b;
-}
-function square_root(a){
-    return a**0.5;
-}
-function square(a){
-    return a**2;
-}
-function factorial(a){
-    let ans = 1;
-    for(let i = 1; i<a; i++){
-        ans *= i;
-    }return ans;
-}
-function remainder(a,b){
-    return a % b;
-}
-function exponent(a,b){
-    return a**b;
-}
-
-function operate(func, a, b){
-    return func(a,b);
 }
 
 // Button functionalities
 
-function clearCache(){
-    num_one = 0;
-    op = "";
-    num_two = 0;
+clear.addEventListener("click",()=>{
+    display.textContent = 0;
+});
+
+del.addEventListener("click",()=>{
+    
+    if(display.textContent.length === 1){
+        display.textContent = 0;
+    }else{
+        display.textContent = display.textContent.slice(0, -1);
+    }
+});
+
+numbers.forEach(number=>{
+    number.addEventListener("click", ()=>{
+        if(display.textContent[0] == 0 && display.textContent.length === 1){
+            display.textContent = number.textContent;
+        }else{
+            display.textContent += number.textContent;
+        }
+    });
+});
+
+function dotInside(str){
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "."){
+            return true;
+        }
+    }return false;
+}
+
+dot.addEventListener("click", ()=>{
+    if(dotInside(display.textContent)){
+        shakeScreen();
+    }else{
+        display.textContent += dot.textContent;
+    }
+});
+
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function remainder(a, b){
+    return a % b;
+}
+
+function operator(func, a, b){
+    return func(a, b);
 }
